7d12e5ce2b23e00bb72c57ee3543033d61f54b01
Bharat Patidar <bharatpatidar399@gmail.com>
2025-06-24 16:13:01 +0530

    yrs added

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -37,7 +37,7 @@ const Job = ({job , index}) => {
                 <p className="text-sm text-gray-600">{job?.description}</p>
             </div>
             <div className="flex items-center gap-2 mt-4 ">
-                <Badge className='text-blue-700 font-bold' variant="ghost">{job?.position}</Badge>
+                <Badge className='text-blue-700 font-bold' variant="ghost">{job?.position} Positions</Badge>
                 <Badge className='text-[#F83002]' variant="ghost">{job?.jobType}</Badge>
                 <Badge className='text-[#7209b7] font-bold' variant="ghost">{job?.salary} LPA</Badge>
             </div>
